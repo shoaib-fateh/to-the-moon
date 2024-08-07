75206147cf57bd2a77c6f6a50f3cb199bce518e6
Shoaib Fateh <shoaibfateh21@yahoo.com>
2024-08-07 20:18:26 +0430

    added skills icons

--- src/pages/home-page/home-page.jsx
+++ src/pages/home-page/home-page.jsx
@@ -5,6 +5,17 @@ import HelloGif from "../../assets/Hello.gif";
 import Illustration from "../../assets/illustration-2.png";
 import AboutImage from "../../assets/about.jpg";
 
+import htmlIcon from "../../assets/html-icon.svg";
+import cssIcon from "../../assets/css-icon.svg";
+import jsIcon from "../../assets/js-icon.svg";
+import bootstrapIcon from "../../assets/bootstrap-icon.svg";
+import nodeIcon from "../../assets/node-icon.svg";
+import reactIcon from "../../assets/react-icon.svg";
+import mysqlIcon from "../../assets/mysql-icon.svg";
+import sassIcon from "../../assets/sass-icon.svg";
+import wordpressIcon from "../../assets/wordpress.svg";
+import typescriptIcon from "../../assets/typescript-icon.svg";
+
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGithub } from "@fortawesome/free-brands-svg-icons/faGithub";
 import { faLinkedin } from "@fortawesome/free-brands-svg-icons/faLinkedin";
@@ -53,7 +64,14 @@ const HomePage = () => (
 		{/* About Section */}
 		<section className="pt-[6rem] pb-[6rem]">
 			<div className="flex flex-col md:flex-row px-5">
-				<div className="flex-1 py-20">
+				<div className="flex-2 px-10">
+					<img
+						src={AboutImage}
+						alt="Illustration"
+						className="w-100 rounded-[5rem] rotate-6 shadow-2xl"
+					/>
+				</div>
+				<div className="flex-2">
 					<div className="font-bold text-blue-600 mb-2 text-3xl">
 						About
 					</div>
@@ -80,13 +98,58 @@ const HomePage = () => (
 					<div className="font-bold text-blue-600 mb-2 text-2xl">
 						Here are my main skills:
 					</div>
-				</div>
-				<div className="flex-1">
-					<img
-						src={AboutImage}
-						alt="Illustration"
-						className="w-100 rounded-[5rem] rotate-6 shadow-2xl"
-					/>
+					<div className="flex gap-1">
+						<img
+							src={htmlIcon}
+							alt="Illustration"
+							className="w-10 shadow-2xl"
+						/>
+						<img
+							src={cssIcon}
+							alt="Illustration"
+							className="w-10 shadow-2xl"
+						/>
+						<img
+							src={jsIcon}
+							alt="Illustration"
+							className="w-10 shadow-2xl"
+						/>
+						<img
+							src={bootstrapIcon}
+							alt="Illustration"
+							className="w-10 shadow-2xl"
+						/>
+						<img
+							src={nodeIcon}
+							alt="Illustration"
+							className="w-10 shadow-2xl"
+						/>
+						<img
+							src={reactIcon}
+							alt="Illustration"
+							className="w-10 shadow-2xl"
+						/>
+						<img
+							src={mysqlIcon}
+							alt="Illustration"
+							className="w-10 shadow-2xl"
+						/>
+						<img
+							src={sassIcon}
+							alt="Illustration"
+							className="w-10 shadow-2xl"
+						/>
+						<img
+							src={wordpressIcon}
+							alt="Illustration"
+							className="w-10 shadow-2xl"
+						/>
+						<img
+							src={typescriptIcon}
+							alt="Illustration"
+							className="w-10 shadow-2xl"
+						/>
+					</div>
 				</div>
 			</div>
 		</section>
